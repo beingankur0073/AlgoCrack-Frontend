470bbdcbf4c38fee85e083bd0818565d04fe7bb3
Ankur Mukherjee <mukherjeeankur0073@gmail.com>
2025-07-06 11:44:20 +0530

    Testing for deployment

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8000/api/v1",
+  // for local 
+  // baseURL: "http://localhost:8000/api/v1", 
+  baseURL: "https://algocrack-backend.onrender.com/api/v1",
   withCredentials: true, 
 });
 
